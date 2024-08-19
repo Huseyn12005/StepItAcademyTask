da18e4a663c9d96f70ddd5746fb17854d1fc71c6
Huseyn <horucov148@gmail.com>
2024-08-19 17:25:05 +0400

    Add project files.

---
+++ StepItAcademyTask/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StepItAcademyTask.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
